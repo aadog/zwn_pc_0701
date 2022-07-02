7319facc837f3f7b7c443e1d33fbc418fb1b8ab1
g.ph <g@google.com>
2022-07-02 22:05:19 +0800

    update

---
+++ go.mod
@@ -0,0 +1,27 @@
+module zwn_pc_0701
+
+go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.11 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/gocolly/colly/v2 v2.1.1-0.20220424184721-cf681331964a // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/nlnwa/whatwg-url v0.1.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/samber/lo v1.21.0 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/willf/bitset v1.1.10 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+	"github.com/samber/lo"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"time"
+)
+var PageMap=sync.Map{}
+var detailList=[]string{}
+func main() {
+
+	ch:=make(chan os.Signal,1)
+	go func() {
+		signal.Notify(ch,os.Kill,os.Interrupt)
+	}()
+	defer func() {
+		<-ch
+	}()
+
+
+	var ul string
+	fmt.Printf("输入url:")
+	fmt.Scanf("%s",&ul)
+	if ul==""{
+		fmt.Println("输入url不正确,结束")
+		return
+	}
+	fmt.Println("开始工作，等待")
+
+
+	c:=colly.NewCollector(colly.Async(false))
+	detailC:=colly.NewCollector(colly.Async(false))
+	detailC.OnHTML(`th:contains("Is this an Adverse Event Report?")`, func(element *colly.HTMLElement) {
+		r:=strings.TrimSpace(element.DOM.Next().Text())
+		if r=="Yes"{
+			fmt.Printf("%s\n",element.Request.URL.String())
+		}else{
+			//fmt.Printf("%s:%s\n",element.Request.URL.String(),"No")
+		}
+	})
+	detailC.OnRequest(func(request *colly.Request) {
+		time.Sleep(time.Millisecond*100)
+	})
+
+	c.OnHTML(`a[href*="detail.cfm?"]`, func(element *colly.HTMLElement) {
+		href:=element.Attr(`href`)
+
+		_,b:=lo.Find(detailList, func(s string)bool {
+			return href==s
+		})
+		if !b{
+			detailList = append(detailList, href)
+			err:=detailC.Visit(element.Request.AbsoluteURL(href))
+			if err!=nil{
+				panic(err)
+			}
+		}
+	})
+	c.OnHTML(`a[href*="results.cfm?"]`, func(element *colly.HTMLElement) {
+		href:=element.Attr(`href`)
+		_,ld:=PageMap.LoadOrStore(href,"1")
+		if !ld{
+			element.Request.Visit(href)
+		}
+	})
+	err:=c.Visit(ul)
+	if err!=nil{
+	    fmt.Println(err)
+	}
+	fmt.Printf("一共搜索到记录:%d条\n", len(detailList))
+}
